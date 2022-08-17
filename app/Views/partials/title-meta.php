2e8f14809d9e9aa7dbf492db8436ebc8817c16c2
tinelipsport <you@example.com>
2022-08-17 11:23:22 +0700

    add view admin

---
+++ app/Views/partials/title-meta.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8" />
+<title><?= $title ? lang('Files.'.$title.'') : '' ?> | Long Film</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta content="Long Film" name="description" />
+<meta content="Themesbrand" name="author" />
+<!-- App favicon -->
+<link rel="shortcut icon" href="/assets/images/favicon.ico">
---
+++ assets/js/pages/jquery-knob.init.js
@@ -0,0 +1,11 @@
+/*
+Long Film
+Author: Themesbrand
+Website: https://themesbrand.com/
+Contact: themesbrand@gmail.com
+File: Jquery knob init Js File
+*/
+
+$(function() {
+    $(".knob").knob();
+});
